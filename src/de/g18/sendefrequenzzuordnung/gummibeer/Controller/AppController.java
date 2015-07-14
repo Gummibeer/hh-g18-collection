13cc313fa3c05cbb1b2ca9d9ed2a166f037056eb
Gummibeer <dev.gummibeer@gmail.com>
2015-07-14 14:04:31 +0200

    controller blueprints

---
+++ src/de/g18/sendefrequenzzuordnung/gummibeer/Controller/AppController.java
@@ -0,0 +1,7 @@
+package de.g18.sendefrequenzzuordnung.gummibeer.Controller;
+
+public class AppController {
+    // TODO: start ImportController
+    // TODO: loop Transmitter-Collection
+    // TODO: create Output-File
+}
---
+++ src/de/g18/sendefrequenzzuordnung/gummibeer/Controller/ImportController.java
@@ -0,0 +1,6 @@
+package de.g18.sendefrequenzzuordnung.gummibeer.Controller;
+
+public class ImportController {
+    // TODO: read yaml file
+    // TODO: transform yaml-output to Transmitter-Collection
+}
