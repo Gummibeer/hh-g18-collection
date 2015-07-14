bf52fc2523d510d288bd40484ca2ff78ecfb419c
Gummibeer <dev.gummibeer@gmail.com>
2015-07-14 14:22:15 +0200

    appcontroller start

--- src/de/g18/sendefrequenzzuordnung/gummibeer/Controller/AppController.java
+++ src/de/g18/sendefrequenzzuordnung/gummibeer/Controller/AppController.java
@@ -2,12 +2,14 @@ package de.g18.sendefrequenzzuordnung.gummibeer.Controller;
 
 import de.g18.sendefrequenzzuordnung.gummibeer.Models.Transmitter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.HashSet;
 
 public class AppController {
 
-    private HashSet<Transmitter> transmitters = new HashSet<>();
+    private List<Transmitter> transmitters = new ArrayList<>();
     public static HashSet<Integer> possibleChannels = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
 
     public void run()
@@ -20,6 +22,9 @@ public class AppController {
     private void loopTransmitters()
     {
         for( Transmitter t : transmitters ) {
+            for( Transmitter t2 : transmitters ) {
+                t.checkTransmitter(t2);
+            }
             for (int c : t.possibleChannels) {
                 if (t.channel == 0 || c < t.channel) {
                     t.channel = c;
